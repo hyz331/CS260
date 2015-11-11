68ff30e2343b59f72b1cc5000d88c6a83c49106d
yunzhong <hyz331@gmail.com>
2015-11-11 12:51:22 -0800

    fix code

--- hw5/CS260_hw5.m
+++ hw5/CS260_hw5.m
@@ -5,21 +5,19 @@ train.data = processData(train.data);
 test.data = processData(test.data);
 
 % Cross validation accuracy
-%Cs = []; accu = [];
-%for i = [-6 -5 -4 -3 -2 -1 0 1 2]
-%    C = 4^i;
-%    [w b] = trainsvm(train.data, train.label, C);
-%    Cs = [Cs; C];
-%    accu = [accu; testsvm(train.data, train.label, w, b)];
-%end
-%plot(Cs, accu);
-%title('Cross validation accuracy');
-%xlabel('C');
-%ylabel('accuracy');
-%[best_accu idx] = max(accu);
-%best_c = Cs(idx)
-%best_accu
-%accu
+Cs = []; accu = [];
+for i = [-6 -5 -4 -3 -2 -1 0 1 2]
+    C = 4^i;
+    tic
+    corr = cross_validation(train.data, train.label, C);
+    toc
+    Cs = [Cs; C];
+    accu = [accu; corr];
+end
+[best_accu idx] = max(accu);
+best_c = Cs(idx)
+best_accu
+accu
 
 % Test accuracy
 %[w b] = trainsvm(train.data, train.label, best_c);
